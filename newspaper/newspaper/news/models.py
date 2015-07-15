45f40dfc65a20386bdfa1f8a5f6e83f5e1d0fd35
Pablo Martín <goinnn@gmail.com>
2015-07-15 19:34:36 +0200

    Clase 2: Creacion del modelo de noticias

--- newspaper/newspaper/news/models.py
+++ newspaper/newspaper/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class News(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    description = models.TextField(_('description'))
+    publish_date = models.DateTimeField(_('publish date'))
+
+    class Meta:
+        verbose_name = _('news item')
+        verbose_name_plural = _('news')
+
+    def __str__(self):
+        return self.title
