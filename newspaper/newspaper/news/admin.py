dce1d6b2003df476efa82c82425217aedbe978c6
Pablo Martín <goinnn@gmail.com>
2015-07-15 19:50:07 +0200

    Clase 2: Creacion de la administracion para los eventos

--- newspaper/newspaper/news/admin.py
+++ newspaper/newspaper/news/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from newspaper.news.models import News
+from newspaper.news.models import News, Event
 
 
 class NewsAdmin(admin.ModelAdmin):
     pass
 
 
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Event, EventAdmin)
