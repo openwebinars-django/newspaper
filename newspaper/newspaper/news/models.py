7f9702d8b5be86b662f88271647572e48a5ffac6
Pablo Martín <goinnn@gmail.com>
2015-07-15 19:47:59 +0200

    Clase 2: Creacion del modelo de eventos

--- newspaper/newspaper/news/models.py
+++ newspaper/newspaper/news/models.py
@@ -4,14 +4,29 @@ from django.utils.translation import ugettext_lazy as _
 
 
 @python_2_unicode_compatible
-class News(models.Model):
+class BaseNews(models.Model):
     title = models.CharField(_('title'), max_length=255)
     description = models.TextField(_('description'))
     publish_date = models.DateTimeField(_('publish date'))
 
     class Meta:
-        verbose_name = _('news item')
-        verbose_name_plural = _('news')
+        abstract = True
 
     def __str__(self):
         return self.title
+
+
+class News(BaseNews):
+
+    class Meta:
+        verbose_name = _('news item')
+        verbose_name_plural = _('news')
+
+
+class Event(BaseNews):
+    start_date = models.DateTimeField(_('start date'))
+    end_date = models.DateTimeField(_('end date'))
+
+    class Meta:
+        verbose_name = _('event')
+        verbose_name_plural = _('events')
