9d92dc8ade2b37268547b93530153fb63085c7b1
Pablo Martín <goinnn@gmail.com>
2015-07-16 19:51:36 +0200

    Clase 3: Creamos primera version vista adicion de noticias

--- newspaper/newspaper/news/urls.py
+++ newspaper/newspaper/news/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('newspaper.news.views',
     url(r'^$', 'news_list', name='news_list'),
+    url(r'^news/add/$', 'news_add', name='news_add'),
+    url(r'^news/edit/(?P<newsitem_pk>\d+)/$', 'news_edit', name='news_edit'),
 )
--- newspaper/newspaper/news/views.py
+++ newspaper/newspaper/news/views.py
@@ -1,12 +1,33 @@
 from datetime import datetime
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+from newspaper.news.forms import NewsForm
 from newspaper.news.models import News
 
 
 def news_list(request):
     news = News.objects.filter(
         publish_date__lte=datetime.now()).order_by('publish_date')
-    return render_to_response("news/news_list.html",
-                              {'news': news})
+    return render_to_response('news/news_list.html',
+                              {'news': news},
+                              context_instance=RequestContext(request))
+
+
+def news_add(request):
+    data = None
+    if request.method == 'POST':
+        data = request.POST
+    news_form = NewsForm(data=data)
+    if news_form.is_valid():
+        news_form.save()
+        return HttpResponseRedirect('../')
+    return render_to_response('news/news_add.html',
+                              {'news_form': news_form},
+                              context_instance=RequestContext(request))
+
+
+def news_edit(request, newsitem_pk):
+    pass
