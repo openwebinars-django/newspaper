60267b25e5171f714e46b870f90a206772ec356c
claudio <claudioac@gmail.com>
2015-07-19 13:58:06 +0200

    mi ejercicio falta datepicker

--- newspaper/newspaper/news/forms.py
+++ newspaper/newspaper/news/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 
 from newspaper.news.models import News
-
+from newspaper.news.models import Event
 
 class NewsForm(forms.ModelForm):
 
     class Meta:
         model = News
         fields = '__all__'
+
+
+
+#formulario de Eventos
+class EventsForm(forms.ModelForm):
+
+    class Meta:
+        model = Event
+        fields = '__all__'        
