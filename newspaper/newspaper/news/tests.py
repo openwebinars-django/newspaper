2a3a7cd45d42ce30b727a00740b30db26d3311a9
Pablo Martín <goinnn@gmail.com>
2015-07-22 19:53:59 +0200

    Clase 6: Mejoro test de adicion de noticias

--- newspaper/newspaper/news/tests.py
+++ newspaper/newspaper/news/tests.py
@@ -50,3 +50,8 @@ class NewsTestCase(TestCase):
         response3 = self.client.get(reverse('news_list'))
         self.assertIn(news_title, response3.content)
         self.assertIn(news_description, response3.content)
+
+        response4 = self.client.post(reverse('news_add'),
+                                     data={})
+        self.assertEqual(response4.status_code, 200)
+        self.assertIn('Este campo es obligatorio.', response4.content)
