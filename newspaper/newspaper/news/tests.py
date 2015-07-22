9771d8ed5427e51e18eea2b849bf1b30e9c46c4a
Pablo Martín <goinnn@gmail.com>
2015-07-22 20:31:06 +0200

    Clase 6: Ahora funcionan nuestros tests con python 3.4, configuracion basica de tox

--- newspaper/newspaper/news/tests.py
+++ newspaper/newspaper/news/tests.py
@@ -22,36 +22,34 @@ class NewsTestCase(TestCase):
         self.client.login(username='admin', password='admin')
 
     def test_newslist(self):
-        news = News.objects.create(title='Mi noticia test',
-                                   description='Mi descripcion test',
-                                   publish_date=datetime.now())
+        News.objects.create(title='Mi noticia test',
+                            description='Mi descripcion test',
+                            publish_date=datetime.now())
         response = self.client.get(reverse('news_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(news.title, response.content)
-        self.assertIn(news.description, response.content)
-        self.assertNotIn('ESTA CADENA NO SE ENCUENTRA EN EL RESPONSE',
+        self.assertIn(b'Mi noticia test', response.content)
+        self.assertIn(b'Mi descripcion test', response.content)
+        self.assertNotIn(b'ESTA CADENA NO SE ENCUENTRA EN EL RESPONSE',
                          response.content)
 
     def test_news_add(self):
         self.authentication()
         response1 = self.client.get(reverse('news_add'))
         self.assertEqual(response1.status_code, 200)
-        self.assertIn('TEXTO DE AYUDA', response1.content)
+        self.assertIn(b'TEXTO DE AYUDA', response1.content)
 
-        news_title = 'Mi noticia test 2'
-        news_description = 'Mi descripcion test 2'
-        data = {'title': news_title,
-                'description': news_description,
+        data = {'title': 'Mi noticia test 2',
+                'description': 'Mi descripcion test 2',
                 'publish_date': datetime.now()}
         response2 = self.client.post(reverse('news_add'),
                                      data=data)
         self.assertEqual(response2.status_code, 302)
 
         response3 = self.client.get(reverse('news_list'))
-        self.assertIn(news_title, response3.content)
-        self.assertIn(news_description, response3.content)
+        self.assertIn(b'Mi noticia test 2', response3.content)
+        self.assertIn(b'Mi descripcion test 2', response3.content)
 
         response4 = self.client.post(reverse('news_add'),
                                      data={})
         self.assertEqual(response4.status_code, 200)
-        self.assertIn('Este campo es obligatorio.', response4.content)
+        self.assertIn(b'Este campo es obligatorio.', response4.content)
