2ef48f26edc4e4b99b262b9384aaaf98c4039dc7
Pablo Martín <goinnn@gmail.com>
2015-07-22 19:22:05 +0200

    Clase 6: Creamos nuestro primer test

--- newspaper/newspaper/news/tests.py
+++ newspaper/newspaper/news/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.client import Client
+
+from newspaper.news.models import News
+
+
+class NewsTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+
+    def test_newslist(self):
+        news = News.objects.create(title='Mi noticia test',
+                                   description='Mi descripcion test',
+                                   publish_date=datetime.now())
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(news.title, response.content)
+        self.assertIn(news.description, response.content)
+        self.assertNotIn('ESTA CADENA NO SE ENCUENTRA EN EL RESPONSE',
+                         response.content)
